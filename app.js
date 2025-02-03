7e953237bb1eb8c2da5fd49954377e1a635134bc
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-03 23:37:30 +0900

    add brush
    1. Canvas initial setup.
    2. Add brush

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-alert("hello")
+const canvas = document.querySelector("canvas");
+
+const brush = canvas.getContext("2d"); 
+
+canvas.width=800;
+canvas.height=800;
+
+brush.fillRect(50, 50, 100, 200);
