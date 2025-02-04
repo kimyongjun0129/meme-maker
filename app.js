8c223e4fbddc6b72d4bcf8ee3a2a51d5e19cf7b1
kimyongjun0129 <kimyongjun0129@naver.com>
2025-02-04 12:17:48 +0900

    implement drawing line
    implement drawing line

--- app.js
+++ app.js
@@ -4,5 +4,26 @@ const brush = canvas.getContext("2d");
 
 canvas.width=800;
 canvas.height=800;
+brush.lineWidth = 2;
+let isPainting = false;
 
-brush.fillRect(50, 50, 100, 200);
+function onMove(event) {
+    if(isPainting) {
+        brush.lineTo(event.offsetX, event.offsetY);
+        brush.stroke();
+    }
+    brush.moveTo(event.offsetX, event.offsetY);
+}
+
+function onMouseDown(event) {
+    isPainting = true;
+}
+
+function cancelPainting(event) {
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", onMouseDown);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
